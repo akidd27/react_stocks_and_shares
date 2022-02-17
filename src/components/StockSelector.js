92057680cf11c76dfccc7d09229d41447c170091
Adam Kidd <adamkidd3@gmail.com>
2022-02-17 19:46:35 +0000

    added onchange

--- src/components/StockSelector.js
+++ src/components/StockSelector.js
@@ -1,12 +1,53 @@
-const StockSelector = () => {
+import React, {useEffect, useState} from 'react';
+
+const StockSelector = ({ onStockSelected }) => {
     
+    const [keywords, setKeywords] = useState("AAPL");
+    const searchUrl = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=H26K0E9MI2QFC8P3`
+    const [searchResults, setSearchResults] = useState([]);
+    const [stockSuggestions, setStockSuggestions] = useState([]);
+
+    const handleChange = ((event) => {
+        const userInput = event.target.value;
+        if (typeof(userInput) == String){
+            setKeywords(userInput);
+        }
+        else{
+            onStockSelected(stockSuggestions[userInput]['1. symbol']);
+        }
+    })
+
+    const updateStockSuggestions = (() => {
+        setStockSuggestions(
+            searchResults.map((stock, index) => {
+                return(
+                    <option value={index} key={index}>
+                        {stock['1. symbol']}: {stock['2. name']} - {stock['4. region']}
+                    </option>
+                )
+            })
+        );
+    })
+
+    useEffect(() => {
+        updateStockSuggestions();
+    }, [searchResults])
+
+    useEffect(() => {
+        if (keywords.length > 1){
+            fetch(searchUrl)
+            .then(result => result.json())
+            .then(data => setSearchResults(data['bestMatches']));
+        }
+    }, [keywords])
+
     return(
         <form>
             <label htmlFor="search-input">Enter a stock's symbol or name</label>
-            <input type="text" id="search-input" list="suggestions"></input>
+            <input type="text" id="search-input" list="suggestions" onChange={handleChange}></input>
             <datalist id="suggestions">
-                <option value="1st option"></option>
-             </datalist>
+                {stockSuggestions}
+            </datalist>
         </form>
     )
 }
--- src/containers/StocksContainer.js
+++ src/containers/StocksContainer.js
@@ -5,14 +5,14 @@ import StockSelector from '../components/StockSelector';
 const StocksContainer = () => {
 
     const [stockData, setStockData] = useState(null);
-    const [stockSymbol, setStockSymbol] = useState("AAPL");
+    const [selectedStockSymbol, setSelectedStockSymbol] = useState("AAPL");
     const [stockPrice, setStockPrice] = useState(0);
     
-    const selectedStockUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=15min&apikey=H26K0E9MI2QFC8P3`;
+    const selectedStockUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${selectedStockSymbol}&interval=15min&apikey=H26K0E9MI2QFC8P3`;
 
     useEffect(() => {
         getData();
-    }, [])
+    }, [selectedStockSymbol])
 
     useEffect(() => {
         if (stockData != null){
@@ -26,10 +26,14 @@ const StocksContainer = () => {
         .then(data => setStockData(data));
     }
 
+    const onStockSelected = (stockSymbol) => {
+        setSelectedStockSymbol(stockSymbol);
+    }
+
     return(
         <div>
-            <StockSelector/>
-            <p>{`${stockSymbol} was last trading at ${stockPrice}$ per share`}</p>
+            <StockSelector onStockSelected={onStockSelected}/>
+            <p>{`${selectedStockSymbol} was last trading at ${stockPrice}$ per share`}</p>
         </div>
     )
 }
